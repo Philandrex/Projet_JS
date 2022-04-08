2f9816b3952b8fd26b5b1243ed6a34f721f160f3
Filip <philippelatif@protonmail.com>
2022-04-08 15:33:57 +0200

    feat: fixed:#8 form added

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 $(document).ready(function () {
-    $('.toggle').on('click',function() {
+    $('.toggle').on('click', function () {
         $('nav').toggle();
     });
 
-    $('.feed').on('click', function() {
-        create();
+    $('.feed').on('click', function () {
+        generate();
     });
+    $("#formulaire").click(function () { 
+       $("form").toggle();
+        
+    });
+    $("#carousel").click(function () { 
+        $(".carousel").toggle();
+         
+     });
+    $("button").on("click", function (event) {
+        event.preventDefault();
+            let table = [{
+            "title": document.getElementById("title").value,
+            "summary": document.getElementById("blog").value,
+            "imageUrl": document.getElementById("url").value,
+        }]
+        feed(table,"#feed");
+    });
+    console.log("fin du programme")
 });
