eb6f93672fa9cb4fd0bb23bafaf6145235d56063
Filip <philippelatif@protonmail.com>
2022-04-12 16:31:27 +0200

    feat: #11 carousel added

--- metodes.js
+++ metodes.js
@@ -101,3 +101,32 @@ if (url == ""){
 }
 return msg;
 }
+
+// nativ carousel
+document.querySelectorAll(".carousel").forEach(carousel =>{
+
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
+
+    carousel.insertAdjacentHTML("beforeend", `
+    <div class="carousel__nav">
+        ${buttonsHtml.join("")}
+    </div>
+    `);
+
+    const buttons = carousel.querySelectorAll(".carousel__button");
+
+    buttons.forEach((button,i)=>{
+        button.addEventListener("click", () =>{
+            items.forEach (item => item.classList.remove("carousel__item--selected"));
+            buttons.forEach(button => button.classList.remove("carousel__button--selected"));
+            items[i].classList.add("carousel__item--selected");
+            buttons[i].classList.add("carousel__button--selected");
+        });
+    });
+
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
+});
