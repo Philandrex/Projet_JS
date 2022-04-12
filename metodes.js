b9a9b60a112d175b6c2d8d8883b26e90d218697a
Filip <philippelatif@protonmail.com>
2022-04-12 10:41:08 +0200

    feat: delete and add image event added

--- metodes.js
+++ metodes.js
@@ -1,16 +1,16 @@
-function feed(params,cible) {
+function feed(params, cible) {
     for (let i = 0; i < params.length; i++) {
         let newDiv = document.createElement("div");
         let title = document.createElement("h2");
         let summary = document.createElement("p");
         let image = document.createElement("img");
-        title.innerHTML = `${params[i].title}`;
-        image.setAttribute("src", `${params[i].imageUrl}`);
+        title.innerHTML = params[i].title;
+        image.setAttribute("src", params[i].imageUrl);
         image.setAttribute("alt", "une photo");
         image.setAttribute("width", 100);
-        summary.innerHTML = `${params[i].summary}`;
+        summary.innerHTML = params[i].summary;
         newDiv.append(title, image, summary);
-        document.querySelector(cible).append(newDiv);
+        document.querySelector(cible).prepend(newDiv);
     }
 }
 
@@ -34,7 +34,41 @@ function generate() {
     document.querySelector("#feed").innerHTML = ""
     fetch(newUrl)
         .then(checkError)
-        .then(result => { feed(result,"#feed") })
+        .then(result => { feed(result, "#feed") })
         .catch(error => { errorMessage(error) });
 }
 
+
+function colonne() {
+    console.log("je suis dans colonne");
+    let elements = document.getElementsByClassName("column");
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "100%";
+        elements[i].style.flex = "100%";
+    }
+}
+function mosaic() {
+    let elements = document.getElementsByClassName("column");
+    console.log(elements);
+    for (let i = 0; i < elements.length; i++) {
+        elements[i].style.msFlex = "25%";
+        elements[i].style.flex = "23%";
+    }
+}
+function addImage(imageUrl, cible) {
+    let image = document.createElement("img");
+    image.setAttribute("src", imageUrl);
+    image.setAttribute("alt", "une image à ajouter");
+    image.setAttribute("width", "23vw");
+    image.setAttribute("class", "column");
+    image.setAttribute("id", "added")
+    document.querySelector(cible).prepend(image);
+}
+function deleteImage(event) {
+    let cible = event.target;
+    let id = cible.getAttribute("id");
+    if (id == "added"){
+    cible.remove();
+    }
+
+}
--- script.js
+++ script.js
@@ -1,27 +1,65 @@
+// document ready event
 $(document).ready(function () {
+
+    // toggle navBar event
     $('.toggle').on('click', function () {
         $('nav').toggle();
     });
 
+    // generate feed 
     $('.feed').on('click', function () {
         generate();
     });
-    $("#formulaire").click(function () { 
-       $("form").toggle();
-        
+
+    // toggle form
+    $("#formulaire").click(function () {
+        $("form").toggle();
+
     });
-    $("#carousel").click(function () { 
+
+    // toggle carousel
+    $("#carousel").click(function () {
         $(".carousel").toggle();
-         
-     });
-    $("button").on("click", function (event) {
+
+    });
+
+    // add blog envent
+    $("#formBlog").on("click", function (event) {
         event.preventDefault();
-            let table = [{
+        let table = [{
             "title": document.getElementById("title").value,
             "summary": document.getElementById("blog").value,
             "imageUrl": document.getElementById("url").value,
         }]
-        feed(table,"#feed");
+        feed(table, "#feed");
     });
-    console.log("fin du programme")
+
+    // add image eve,t
+    document.getElementById("imageForm").addEventListener("click", function (event) {
+        event.preventDefault();
+        let imageUrl = document.getElementById("image").value;
+        addImage(imageUrl, ".row");
+    });
+
+    // delete image event
+    document.addEventListener("dblclick",deleteImage);
+
+    // mosaic click event
+    document.getElementById("mosaic").addEventListener("click", mosaic);
+
+    // colonne click event
+    document.getElementById("colonne").addEventListener("click", colonne);
+
+    // btn control on click event
+    let main = document.getElementById("myMain");
+    let btns = main.getElementsByClassName("btn");
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function () {
+            let current = document.getElementsByClassName("active");
+            current[0].className = current[0].className.replace(" active", "");
+            this.className += " active";
+        });
+    }
+
+    console.log("je suis la")
 });
